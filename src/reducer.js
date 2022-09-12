8aad6b05f07522fbc2b24598278b33d8e25f697d
jmoua123 <nraugnaagmouaa@gmail.com>
2022-09-11 18:05:57 -0700

    Cleaned Reducer

--- src/reducer.js
+++ src/reducer.js
@@ -2,12 +2,8 @@ export const initialState = {
     basket: [],
     user: null
   };
-  
-  // Selector
-  export const getBasketTotal = (basket) => 
-    basket?.reduce((amount, item) => item.price + amount, 0);
-  
-  const reducer = (state, action) => {
+    
+    const reducer = (state, action) => {
     console.log(action);
     switch (action.type) {
       
